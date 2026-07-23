4dfb8f3a76c6a13f92f21a601ac7929cae92588a
MidnightHawkEye <dario.werner.hasler@gmail.com>
2026-07-23 20:00:00 +0200

    Small adjustment for the mobil Version
    - small refresh bugs
    - issues list Readme.md

--- js/loader.js
+++ js/loader.js
@@ -1,14 +1,3 @@
-/*--------------------- Loader ---------------------*/
-/*
-window.onload = () => {
-    gsap.to("#loader", {
-        opacity:0,
-        duration:1.5,
-        onComplete: () => {
-            document.getElementById("loader").style.display = "none";
-        }
-    });
-};*/
 
 /*==================================================
                     LOADER
